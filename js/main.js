cc5a8c02fd532a872e20b321d220dd6c7d7e9402
jetzerszeri <jetzers.2@gmail.com>
2023-10-01 14:09:47 -0400

    update: home hero img

--- js/main.js
+++
@@ -1,7 +0,0 @@
-//navbar
-const togglebtn = document.querySelector('.toggle-button');
-const navbarlinks = document.querySelector('.navbar-links');
-
-togglebtn.addEventListener('click', () => {
-    navbarlinks.classList.toggle('active');
-});
