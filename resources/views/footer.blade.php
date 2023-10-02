e72558fc63ac17458bc4e6a7771ead74ec4828fe
jetzerszeri <jetzers.2@gmail.com>
2023-10-02 18:38:42 -0400

    feat: making header and footer as partials files

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+    <div>
+        <ul>
+            <li><a href="" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+            <li><a href="" target="_blank"><i class="fa-brands fa-x-twitter"></i></i></a></li>
+            <li><a href="" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+        </ul>
+        <div>
+            <p>gbygpcs.com</p>
+            <p>© 2023 Copyright</p>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,27 @@
+<header class="navegacion">
+    <nav >
+        <div class="navbar">
+            <div>
+                <div class="navbarlogo">GbyG PCS</div>
+
+                <div class="toggle-button">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </div>
+                
+            </div>
+
+            <div class="navbar-links">
+                <ul>
+                    <li><a href="/" class="active">Inicio</a></li>
+                    <li><a href="/services" class="">Servicios</a></li>
+                    <!-- <li><a href="#" class="">Dashboard</a></li> -->
+                </ul>
+                <div>
+                    <a href="#" class="btn">Iniciar sesión</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
