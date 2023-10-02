55a869e2587c8a0b80fd227cd2a2f6d4c07c6900
jetzerszeri <jetzers.2@gmail.com>
2023-10-02 18:39:13 -0400

    feat: adding index view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layout')
+
+@section('content')
+    <main class="homecontainer">
+        <section class="heroHome">
+            <div>           
+                <div>
+                    <h1>El mejor servicio de limpieza de la ciudad</h1>
+                    <p>Personalizado a tus necesidades, comprometidos con la excelencia.</p>
+                    <a href="services.html" class="btn primary-yellow">Ver servicios</a>
+                </div>
+                <div>
+                    <img src="img/herohome1.webp" alt="imagen de una mano con guante amarillo de limpieza agarrando un pañuelo que está sobre el lavabo de un baño">
+                </div>
+            </div>
+        </section>
+
+        <section class="homeservices">
+            <div>
+                <h2>Servicos profesionales de limpieza</h2>
+                <p>Especializados en:</p>
+
+                <div>
+                    <div class="card">
+                        <img src="img/postreno1.webp" alt="habitacion en remodelación">
+                        <p>Post remodelación</p>
+                        <a href="services.html">Ver servicios</a>
+                    </div>
+                    <div class="card">
+                        <img src="img/homemove.webp" alt="cajas de mudanza">
+                        <p>Move in/Move out</p>
+                        <a href="services.html">Ver servicios</a>
+                    </div>
+                    <div class="card">
+                        <img src="img/homemantenimiento.webp" alt="Cocina de una casa en venta">
+                        <p>Mantenimiento</p>
+                        <a href="services.html">Ver servicios</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div>
+                <h2>¿Necesitas un servicio personalizado?</h2>
+                <p>Dinos cómo podemos ayudarte</p>
+                <a href="services.html" class="btn primary-green">Contáctanos</a>
+            </div>
+
+        </section>
+
+    </main>
+@endsection
