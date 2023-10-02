b57d040795501315d2b13e90221af5907b474bbb
jetzerszeri <jetzers.2@gmail.com>
2023-10-02 18:42:49 -0400

    removing initial maqueta

--- maqueta/js/main.js
+++
@@ -1,6 +0,0 @@
-const togglebtn = document.querySelector('.toggle-button');
-const navbarlinks = document.querySelector('.navbar-links');
-
-togglebtn.addEventListener('click', () => {
-    navbarlinks.classList.toggle('active');
-})
