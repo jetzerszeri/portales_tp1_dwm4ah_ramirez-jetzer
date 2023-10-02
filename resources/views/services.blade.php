1eb0e5cb3ac178752ca517476281babb6e584738
jetzerszeri <jetzers.2@gmail.com>
2023-10-02 18:39:30 -0400

    feat: adding services view

---
+++ resources/views/services.blade.php
@@ -0,0 +1,73 @@
+@extends('layout')
+
+@section('content')
+
+
+<main class="servicescontainer">
+    <header class="servicesheader">
+        <div>
+            <h1>Servicios profesionales de limpieza</h1>
+            <p>Elevando el estandar de limpieza con soluciones personalizadas.</p>
+        </div>
+
+        <div>
+            <ul>
+                <li><a href="" class="btn secundary-green">Remodelación</a></li>
+                <li><a href="" class="btn secundary-green">Mudanza</a></li>
+                <li><a href="" class="btn secundary-green">Real State</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <section>
+        <ul>
+            <li> 
+                <div>
+                    <div>
+                        <img src="img/servicio1.webp" alt="escalera y herramientas de pintura en una habitación en construcción">
+                    </div>
+                    <div>
+                        <div>
+                            <h2>Limpieza post remodelación / construcción</h2>
+                            <p>Remodelación</p>
+                        </div>
+                        
+                        <a href="/servicio" class="btn secundary-green">Ver detalles</a>
+
+                    </div>
+                </div>
+            </li>
+            <li> 
+                <div>
+                    <div>
+                        <img src="img/servicio1.webp" alt="escalera y herramientas de pintura en una habitación en construcción">
+                    </div>
+                    <div>
+                        <div>
+                            <h2>Limpieza post remodelación</h2>
+                            <p>Remodelación</p>
+                        </div>
+                        <a href="/servicio" class="btn secundary-green">Ver detalles</a>
+
+                    </div>
+                </div>
+            </li>
+            <li> 
+                <div>
+                    <div>
+                        <img src="img/servicio1.webp" alt="escalera y herramientas de pintura en una habitación en construcción">
+                    </div>
+                    <div>
+                        <div>
+                            <h2>Limpieza post remodelación</h2>
+                            <p>Remodelación</p>
+                        </div>
+                        <a href="/servicio" class="btn secundary-green">Ver detalles</a>
+
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </section>
+</main>
+@endsection
