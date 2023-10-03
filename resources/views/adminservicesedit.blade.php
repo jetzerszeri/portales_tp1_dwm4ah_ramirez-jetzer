4c19fd19ea24677baf2d726782331824743fa0c6
jetzerszeri <jetzers.2@gmail.com>
2023-10-03 09:07:11 -0400

    refactor: make the service form as a partial

--- resources/views/adminservicesedit.blade.php
+++ resources/views/adminservicesedit.blade.php
@@ -17,38 +17,7 @@
         <li><a href="" class="btn">Agregar un servicio</a></li>
     </ul>
 
+    @include('serviceform')
 
-    <form action="" method="post">
-        @csrf
-        <div>
-            <label for="name">Nombre del servicio</label>
-            <input type="text" placeholder="Ingresa el nombre del servicio" name="name" value="{{ old('name', $service->name)}}">
-            <p>{{ $errors->first('name') }}</p>
-        </div>
-        <div>
-            <label for="category">Categoría</label>
-            <select name="category">
-                <option value="1" @selected(old("category") == 1)>Remodelacion</option>
-                <option value="2" @selected(old("category") == 2)>Move in/Move out</option>
-                <option value="3" @selected(old("category") == 3)>Real State</option>
-            </select>
-            {{ $errors->first('category') }}
-        </div>
-        <div>
-            <label for="description">Descripción</label>
-            <textarea name="description" cols="30" rows="10">{{ old('description', $service->description)}}</textarea>
-            <p>{{ $errors->first('description') }}</p>
-        </div>
-
-        <div>
-            <label for="img">Imagen</label>
-            <input type="text" placeholder="Ingresa el url de la imagen" name="img" value="{{ old('img', $service->img)}}">
-            <p>{{ $errors->first('img') }}</p>
-        </div>
-
-        <div>
-            <button type="submit" class="btn primary-green">Agregar usuario</button>
-        </div>
-    </form>
 </main>
 @endsection
