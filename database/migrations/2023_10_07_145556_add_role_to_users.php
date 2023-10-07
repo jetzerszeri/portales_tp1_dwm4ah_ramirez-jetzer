c55db4bf322af27651e5cfc151936e282ffca6e1
jetzerszeri <jetzers.2@gmail.com>
2023-10-07 11:01:17 -0400

    adding the lastname, role, and sofdeletes columns to users table
    this needs to be done on the migration previously created.
    
    to update the table/migrations, run: 'php artisan migrate' command

--- database/migrations/2023_10_07_145556_add_role_to_users.php
+++ database/migrations/2023_10_07_145556_add_role_to_users.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('lastname', 100);
+            $table->string('role', 50);
+            $table->softDeletes();
         });
     }
 
