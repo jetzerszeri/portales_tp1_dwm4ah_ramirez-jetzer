d490fbe41f9a00ae0959e314b7bdba9684192243
jetzerszeri <jetzers.2@gmail.com>
2023-10-07 15:12:01 -0400

    refactor: make adminlayout dynamic to reuse it

--- resources/views/adminlayout.blade.php
+++ resources/views/adminlayout.blade.php
@@ -1,22 +1,34 @@
 @extends('layout')
 @section('content')
 <main class="adminmain">
+
+    @php
+        $entity = request()->segment(2);
+        $entitiesMap = [
+            'services' => 'Servicios',
+            'users' => 'Usuarios',
+            'categories' => 'Categorías',
+            'requests' => 'Solicitudes',
+        ];
+        $currentEntityName = $entitiesMap[$entity] ?? 'Dashboard';
+    @endphp
+
     <nav class="breadcrumb">
-            <ol>
-                <li><a href="/admin">Dashboard</a></li>
-                @unless($h2 == 'Servicios')
-                    <li><a href="/admin/services">Servicios</a></li>
-                @endunless
-                <li aria-current="page">{{$h2}}</li>
-            </ol>
+        <ol>
+            <li><a href="/admin">Dashboard</a></li>
+            @if($currentEntityName !== $h2)
+                <li><a href="/admin/{{$entity}}">{{$currentEntityName}}</a></li>
+            @endif
+            <li aria-current="page">{{$h2}}</li>
+        </ol>
     </nav>
 
     <h1>Panel de administrador</h1>
     <h2>{{$h2}}</h2>
 
     <ul class="usersOptions">
-        <li><a href="/admin/services" class="btn">Mostrar todos</a></li>
-        <li><a href="/admin/services/add" class="btn">Agregar uno</a></li>
+        <li><a href="/admin/{{$entity}}" class="btn">Mostrar todos</a></li>
+        <li><a href="/admin/{{$entity}}/add" class="btn">Agregar uno</a></li>
     </ul>
 
     @yield('subcontent')
