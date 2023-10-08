1dfd53972de7e629f516d00263854a834e064654
jetzerszeri <jetzers.2@gmail.com>
2023-10-07 21:52:23 -0400

    feat: create migration for requests table
    php artisan make:migration create_requests

---
+++ database/migrations/2023_10_08_013007_create_requests.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20);
+            $table->string('lastname', 20);
+            $table->string('email', 100);
+            $table->string('address', 100);
+            $table->string('city', 30);
+            $table->tinyInteger('state_id');
+            $table->string('zip_code', 5);
+            $table->unsignedBigInteger('service_id');
+            $table->date('service_date');
+            $table->string('notes', 1000);
+            $table->timestamps();
+            $table->softDeletes();
+            
+            $table->foreign('service_id')->references('id')->on('services');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
