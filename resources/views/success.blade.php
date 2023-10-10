c5b2626bf01e384e036aef21c368569e71254e2e
jetzerszeri <jetzers.2@gmail.com>
2023-10-10 10:37:40 -0400

    style: update css for success message

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -3,7 +3,7 @@
 
 <main>
 
-<div>
+<div class="successMsg">
     <h1>Gracias por tu solicitud!</h1>
     <p>En breve nos pondremos en contacto contigo para darte los detalles del presupuesto para tu solicitud.</p>
     <a href="/" class="btn">Volver al inicio</a>
