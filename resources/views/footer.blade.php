b2a96f1e8aa8c9ad578ed3720079f7f0c61f01f2
jetzerszeri <jetzers.2@gmail.com>
2023-10-10 09:58:23 -0400

    update: footer links

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,9 @@
 <footer>
     <div>
         <ul>
-            <li><a href="" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
-            <li><a href="" target="_blank"><i class="fa-brands fa-x-twitter"></i></i></a></li>
-            <li><a href="" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+            <li><a href="https://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+            <li><a href="https://twitter.com" target="_blank"><i class="fa-brands fa-x-twitter"></i></i></a></li>
+            <li><a href="https://www.instagram.com" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
         </ul>
         <div>
             <p>gbygpcs.com</p>
