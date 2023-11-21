3f868120ae546ea2d09cd7ce5ad1ff893b368c54
jetzerszeri <jetzers.2@gmail.com>
2023-11-20 22:11:19 -0500

    feat: add the down method on the migration of the notes column

--- database/migrations/2023_10_08_024230_modify_notes_column_on_requests_table_again.php
+++ database/migrations/2023_10_08_024230_modify_notes_column_on_requests_table_again.php
@@ -22,6 +22,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('requests', function (Blueprint $table) {
+            $table->text('notes')->nullable()->change();
+        });
+        
     }
 };
