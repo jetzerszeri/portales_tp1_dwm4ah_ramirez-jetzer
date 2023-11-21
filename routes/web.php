ac369a1560ad7f1906017c20083567243f7f5856
jetzerszeri <jetzers.2@gmail.com>
2023-11-20 20:37:36 -0500

    refactor: contact router, removing contact controller

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,19 @@
 
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Service;
 use App\Models\User;
 use App\Models\Category;
 use App\Models\Request as RequestModel;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 use App\Models\State;
 
 use App\Http\Requests\LoginUser;
+use App\Http\Requests\CreateRequest;
 
 use App\Http\Controllers\Customer\ServicesController;
 use App\Http\Controllers\Customer\RequestsController;
-use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Admin\UsersController;
 use App\Http\Controllers\Admin\ServicesController as AdminServicesController;
 use App\Http\Controllers\Admin\CategoriesController;
@@ -29,7 +28,6 @@ Route::get('/login', function () { return view('login');})->name('login');
 
 Route::resource('services', ServicesController::class);
 Route::resource('requests', RequestsController::class);
-Route::resource('contact', ContactController::class);
 Route::resource('admin/users', UsersController::class)->middleware('auth');
 Route::resource('admin/services', AdminServicesController::class)->middleware('auth');
 Route::resource('admin/categories', CategoriesController::class)->middleware('auth');
@@ -43,7 +41,6 @@ Route::get('/admin', function ( Request $request ) {
             'username' => $currentUser
         ]);
 })->middleware('auth');
-
 
 Route::get('/logout', function(){
     Auth::logout();
@@ -59,4 +56,21 @@ Route::post('/login', function ( LoginUser $request) {
     } else {
         return redirect('/login')->withErrors(['loginError' => 'Credenciales inválidas.']);
     }
+});
+
+Route::get('/contact', function () {
+    $servicesList = Service::all();
+    $statesList = State::all();
+    return view('contact', [
+        'servicesList' => $servicesList,
+        'h2' => '¡Compartenos tu inquietud!',
+        'label_nota' => 'Coméntanos, ¿cómo podemos ayudarte?',
+        'statesList' => $statesList, 
+    ]);
+});
+
+Route::post('/contact', function (CreateRequest $request) {
+    $data = request()->all();
+    RequestModel::create($data);
+    return redirect('/success');
 });
