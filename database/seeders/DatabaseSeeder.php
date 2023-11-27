7cc55e235ccb3e21a37ed0625d43665e15ddd8a5
jetzerszeri <jetzers.2@gmail.com>
2023-11-27 05:55:29 -0500

    feat: create CategoryFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,16 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // User::truncate();
+        // Category::truncate();
+
+        \App\Models\Category::factory(5)->create();
 
         $this->call([
-            CategoriesSeeder::class,
-            ServicesSeeder::class,
-            UsersSeeder::class,
-            RequestsSeeder::class,
-            StatesSeeder::class,
-            RolesSeeder::class,
+            // CategoriesSeeder::class,
+            // ServicesSeeder::class,
+            // UsersSeeder::class,
+            // RequestsSeeder::class,
+            // StatesSeeder::class,
+            // RolesSeeder::class,
         ]);
-        // \App\Models\User::factory(10)->create();
+        
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
